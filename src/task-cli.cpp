6ce78e3fc3988cc73f960e63af0b22e0ac69f1d8
Toprak Ekin Yüksel <ekinyuksel12@gmail.com>
2025-09-18 22:01:49 +0300

    add first versions of 'help', 'add' and 'list'
    This commit adds the functionallity of adding a new task, listing currently saved tasks and a help dialouge.
    These functionality have basic forms now. Will update later.

---
+++ src/task-cli.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <cxxopts.hpp>
+#include <nlohmann/json.hpp>
+#include <task.hpp>
+
+using namespace std;
+using json = nlohmann::json;
+
+void saveTask (Task task) {
+    ifstream iFile("example.json");
+    json j = json::parse(iFile);
+    iFile.close();
+
+    ofstream oFile("example.json");
+    cout << j << endl;
+
+    task.id = j.size();
+    j.push_back(task);
+
+    oFile << j.dump(4);
+    oFile.close();
+}
+
+void createNewTask (string description) {
+    Task newTask;
+    newTask.setDescription(description);
+
+    saveTask(newTask);
+}
+
+int main (int argc, char* argv[]){
+    //Create a cxxopt Options object and add options to it.
+    cxxopts::Options options ("TaskTracker", "A command line todo/task tracker written in C++ ");
+
+    options.add_options()
+        ("a,add", "Create a new task with given description", cxxopts::value<std::string>())
+        ("l,list", "List all tasks")
+        ("h,help", "Print help screen");
+    
+    auto result = options.parse(argc, argv);
+
+    //Handling the --help flag.
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    if (result.count("add")) {
+        string description = result["add"].as<std::string>();
+        createNewTask(description);
+    }
+
+    if (result.count("list")) {
+        ifstream jsonFile("example.json");
+        json j = json::parse(jsonFile);
+
+        std::vector<Task> tasks = j.get<std::vector<Task>>();
+
+        for (auto& task: tasks)
+        {
+            cout << task << endl;
+        }
+    }
+}
